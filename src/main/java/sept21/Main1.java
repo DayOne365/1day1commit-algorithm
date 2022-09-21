d3acf23c52d6b11fa55ee808fbf22690b785b986
DayOne365 <dsh0304@gmail.com>
2022-09-21 09:47:20 +0900

    [알고리즘] - 4번 단어 뒤집기
    변경사항 :
    
    - String 문자열을 char 배열로 변경
    - 맨 왼쪽 단어와 맨 오른쪽 단어를 변수로 저장
    - while문을 통해 조건(왼쪽 < 오른쪽)이 될때까지 자리 교체

---
+++ src/main/java/sept21/Main1.java
@@ -0,0 +1,62 @@
+package sept21;
+
+
+// 4. 단어 뒤집기
+//설명
+//N개의 단어가 주어지면 각 단어를 뒤집어 출력하는 프로그램을 작성하세요.
+
+//입력
+//첫 줄에 자연수 N(3<=N<=20)이 주어집니다.
+
+//두 번째 줄부터 N개의 단어가 각 줄에 하나씩 주어집니다. 단어는 영어 알파벳으로만 구성되어 있습니다.
+
+//출력
+//N개의 단어를 입력된 순서대로 한 줄에 하나씩 뒤집어서 출력합니다.
+
+// 예시 입력
+//3
+//good
+//Time
+//Big
+
+// 예시 출력
+//doog
+//emiT
+//giB
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main1 {
+    public ArrayList<String> solution(int n, String[] str) {
+        ArrayList<String> result = new ArrayList<String>();
+
+        for (String x : str) {
+        String temp = new StringBuilder(x).reverse().toString();
+        result.add(temp);
+        }
+        return result;
+    }
+    public static void main(String[] args) {
+        Main1 T = new Main1();
+        Scanner kb = new Scanner(System.in);
+        //첫 줄에 자연수 N(3<=N<=20)이 주어집니다.
+        // 뒤집을 단어 갯수 정하기
+        int n = kb.nextInt();
+        // 위에 입력받은 변수 n을 동적 배열로 활용
+        // 동적 배열 만들기
+        // 자바에서는 배열의 칸 수를 정해야 한다.
+        String[] str = new String[n];
+
+        //두 번째 줄부터 N개의 단어가 각 줄에 하나씩 주어집니다. 단어는 영어 알파벳으로만 구성되어 있습니다.
+        // 아름답다! 반복문은 0으로 초기화한 i로부터 n만큼 돌린다
+        for (int i = 0; i < n; i++) {
+            // 입력 함수로 받은 단어들을 str 배열에 넣는다
+            str[i] = kb.next();
+        }
+        for (String x : T.solution(n, str)) { // T.solution(n, str) = 뒤집힌 단어로 바뀌어야한다.
+            System.out.println(x);
+        }
+    }
+
+}
---
+++ src/main/java/sept21/Main2.java
@@ -0,0 +1,57 @@
+package sept21;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main2 {
+
+    public ArrayList<String> solution(String[] str) {
+        ArrayList<String> result = new ArrayList<>();
+
+        // String 타입에 문자배열 메소드가 있다
+
+        for (String x : str) {
+            char[] c = x.toCharArray();
+            int lt = 0, rt = x.length() - 1; // lt = 왼쪽 맨 끝 문자, rt = 오른쪽 맨 끝 문자
+            while (lt < rt) {
+                // abcd 를 받았다고 했을 때
+                // lt[0] = a; rt[3] = d
+                // tmp = a;
+                // c[0] = c[rt] = d
+                // c[0] = d = tmp;
+                // c[3] = tmp = d;
+
+                // 문자열 good
+                // lt = g , rt = d;
+                char tmp = c[lt];
+                System.out.println("1번 여기서 템프의 값은 ? : "+  tmp);
+                c[lt] = c[rt]; // 절차지향적으로 읽어 내려오기때문에 // 여기서 c[rt]의 값을 c[lt] 값으로 변경
+                System.out.println("2번 여기서 템프의 값은 ? :" + tmp);
+                c[rt] = tmp; // 헷갈렸던건 c[rt]의 값을 c[lt] 값 변경으로 tmp도 값이 바뀌는 줄 알았는데 아니였다. 즉, c[0] = s 일때 값을 그대로 tmp가 저장해서 c[rt]로 넣어준다.
+                System.out.println("3번 여기서 c[rt]의 값은 ? :" + c[rt]);
+
+                lt++;
+                rt--;
+            }
+            String tmp = String.valueOf(c); // 일반적으로 인스턴스(객체). <- 으로해서 메서드를 참조하지만, valueOf는 스태틱이라서 클래스. <-으로 접근가능하다
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+
+        Main2 main2 = new Main2();
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        String[] str = new String[n];
+
+        for (int i = 0; i < n; i++) {
+            str[i] = kb.next();
+        }
+
+        for (String x : main2.solution(str)) {
+            System.out.println(x);
+        }
+
+    }
+}
